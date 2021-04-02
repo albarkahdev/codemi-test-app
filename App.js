491feb92bc45e84eccf68eaf7e0cf014e3366304
Ahsan Shibghotullah <albarkahdev@gmail.com>
2021-04-02 14:14:20 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+
+import type {Node} from 'react';
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View />
+  );
+};
+
+export default App;
